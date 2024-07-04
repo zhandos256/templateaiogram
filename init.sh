38bc7561b19b4e6f390d375d29b51f544aa56286
zhandos256 <r.zhandos256@gmail.com>
2024-07-04 20:11:01 +0500

    some refs

--- init.sh
+++ init.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-. ./.env/bin/activate
+# Create new python env
+python3 -m venv .env
+
+# Create folders
+mkdir src/migrations/versions
+mkdir src/locales
+
+# Activate env
+source ./.env/bin/activate
+
+# Install requirements
+pip install -U pip
+pip install -r requirements.txt
+
+# Go to src
 cd src
+
+# Init DB
 python init_db.py
+
+# Create initial migrations
 alembic revision --autogenerate -m "initial"
 alembic upgrade head
--- venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir src/migrations/versions
-mkdir src/locales
-python3 -m venv .env
-source ./.env/bin/activate
-pip install -U pip
-pip install -r requirements.txt
