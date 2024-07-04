0ad0a3a66d1a1ebf4d4ee239dee08384dac9615b
zhandos256 <r.zhandos256@gmail.com>
2024-07-04 20:01:17 +0500

    some refs

--- src/core/const.py
+++ src/core/const.py
@@ -6,5 +6,5 @@ TOKEN = environ.get('TEST_BOT_TOKEN')
 BASE_DIR = Path(__file__).parent.parent
 LOCALES_DIR = BASE_DIR / 'locales'
 DB_PATH = BASE_DIR / 'db.sqlite'
-DB_URL = f'sqlite+aiosqlite:///{DB_PATH}'
+SQLITE_URL = f'sqlite+aiosqlite:///{DB_PATH}'
 POSTGRES_URL = 'postgresql+asyncpg://postgres:123@localhost:5423/postgres'
