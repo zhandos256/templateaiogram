c9334019fcdb5c63564b9c275dfd7165f48ec013
zhandos256 <r.zhandos256@gmail.com>
2024-07-04 19:52:10 +0500

    some refs

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 . ./.env/bin/activate
 cd src
-mkdir migrations/versions
 python init_db.py
 alembic revision --autogenerate -m "initial"
 alembic upgrade head
--- venv.sh
+++ venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir src/migrations/versions
 python3 -m venv .env
 source ./.env/bin/activate
 pip install -U pip
