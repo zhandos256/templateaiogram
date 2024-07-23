77b29d6b922f72408a7d520797c7600e9484689c
zhandos256 <r.zhandos256@gmail.com>
2024-07-24 01:35:49 +0500

    some refs

--- src/core/const.py
+++ src/core/const.py
@@ -2,7 +2,7 @@ from os import environ
 from pathlib import Path
 
 DEBUG = 0
-TOKEN = environ.get('TEST_BOT_TOKEN')
+BOT_TOKEN = environ.get('TEST_BOT_TOKEN')
 BASE_DIR = Path(__file__).parent.parent
 LOCALES_DIR = BASE_DIR / 'locales'
 DB_PATH = BASE_DIR / 'db.sqlite'
